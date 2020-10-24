46366dee3e032b51292b1b1beb916385bf4137cf
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-10-23 17:40:15 -0700

    Port `fromString` from Number.purs into Gobal.purs

--- src/Global.purs
+++ src/Global.purs
@@ -7,6 +7,7 @@ module Global
   , isFinite
   , readInt
   , readFloat
+  , fromString
   , toFixed
   , toExponential
   , toPrecision
@@ -33,6 +34,39 @@ foreign import readInt :: Int -> String -> Number
 
 -- | Parse a floating point value from a `String`
 foreign import readFloat :: String -> Number
+
+-- | Attempt to parse a `Number` using JavaScripts `parseFloat`. Returns
+-- | `Nothing` if the parse fails or if the result is not a finite number.
+-- |
+-- | Example:
+-- | ```purs
+-- | > fromString "123"
+-- | (Just 123.0)
+-- |
+-- | > fromString "12.34"
+-- | (Just 12.34)
+-- |
+-- | > fromString "1e4"
+-- | (Just 10000.0)
+-- |
+-- | > fromString "1.2e4"
+-- | (Just 12000.0)
+-- |
+-- | > fromString "bad"
+-- | Nothing
+-- | ```
+-- |
+-- | Note that `parseFloat` allows for trailing non-digit characters and
+-- | whitespace as a prefix:
+-- | ```
+-- | > fromString "  1.2 ??"
+-- | (Just 1.2)
+-- | ```
+fromString :: String -> Maybe Number
+fromString = readFloat >>> check
+  where
+    check num | isFinite num = Just num
+              | otherwise    = Nothing
 
 foreign import _toFixed :: forall a. Fn4 (String -> a) (String -> a) Int Number a
 
