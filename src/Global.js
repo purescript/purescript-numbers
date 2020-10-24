db8e0a86d41e7c9eb01fe1338b575e83920608ce
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-10-23 17:44:58 -0700

    Copy over Global.purs from `globals` but exclude URI-related code

---
+++ src/Global.js
@@ -0,0 +1,33 @@
+/* globals exports */
+"use strict";
+
+exports.nan = NaN;
+
+exports.isNaN = isNaN;
+
+exports.infinity = Infinity;
+
+exports.isFinite = isFinite;
+
+exports.readInt = function (radix) {
+  return function (n) {
+    return parseInt(n, radix);
+  };
+};
+
+exports.readFloat = parseFloat;
+
+var formatNumber = function (format) {
+  return function (fail, succ, digits, n) {
+    try {
+      return succ(n[format](digits));
+    }
+    catch (e) {
+      return fail(e.message);
+    }
+  };
+};
+
+exports._toFixed = formatNumber("toFixed");
+exports._toExponential = formatNumber("toExponential");
+exports._toPrecision = formatNumber("toPrecision");
